ad29816d26a834d23eca418fbbe44ddbe7bf74e6
SATHVIK SVS <svssathvik77@gmail.com>
2025-02-06 21:31:27 +0530

    update toaster max length

--- src/utils/toaster.ts
+++ src/utils/toaster.ts
@@ -1,16 +1,19 @@
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
+const formatMessage = (message: string) => {
+  const returnMessage = message.slice(0, 20);
+  return returnMessage;
+};
 const toaster = (type: string, message: string) => {
-    if (type === "success") {
-        toast.success(message);
-    } else if (type === "error") {
-        toast.error(message);
-    } else if (type === "custom") {
-        toast.custom(message);
-    }
-    else {
-        toast(message);
-    }
+  if (type === "success") {
+    toast.success(formatMessage(message));
+  } else if (type === "error") {
+    toast.error(formatMessage(message));
+  } else if (type === "custom") {
+    toast.custom(formatMessage(message));
+  } else {
+    toast(formatMessage(message));
+  }
 };
 
 export default toaster;
